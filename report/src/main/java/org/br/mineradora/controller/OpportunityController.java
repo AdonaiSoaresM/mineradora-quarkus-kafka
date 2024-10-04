13af6954ea6dc47e6f391095c82e3b432e1a01e6
AdonaiSoaresM <adonaielite@gmail.com>
2024-10-03 22:00:45 -0300

    Report Microservicos: API + Kafka + BD

---
+++ report/src/main/java/org/br/mineradora/controller/OpportunityController.java
@@ -0,0 +1,33 @@
+package org.br.mineradora.controller;
+
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import org.br.mineradora.dto.OpportunityDTO;
+import org.br.mineradora.service.OpportunityService;
+
+
+import java.util.List;
+
+@Path("/api/opportunity")
+//@Authenticated
+public class OpportunityController {
+
+//    @Inject
+//    JsonWebToken token;
+
+    @Inject
+    OpportunityService opportunityService;
+
+    @GET
+    @Path("/data")
+//    @RolesAllowed({"user","manager"})
+    public List<OpportunityDTO> generateReport(){
+
+        return opportunityService.generateOpportunityData();
+
+    }
+
+}
